5aad793a1f428e2ebef623448b9b98f4a83d1c2a
Saenaru <Nyanja230@gmail.com>
2025-06-14 23:38:28 +0300

    Customize Flat Admin

--- property/admin.py
+++ property/admin.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Flat
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = [
-        'town',
-        'address',
-        'owner'
-    ]
-    list_display = [
-        'address',
-        'price',
-        'town',
-        'owner'
+    search_fields = ['town', 'address', 'owner']
+    list_display = ['address', 'price', 'town', 'owner', 'created_at']
+    list_filter = ['created_at']
+    readonly_fields = ['created_at']
+    fieldsets = [
+        (None, {
+            'fields': ['owner', 'owners_phonenumber']
+        }),
+        ('Информация о квартире', {
+            'fields': ['description', 'price', 'town', 'town_district', 
+                      'address', 'floor', 'rooms_number', 'living_area',
+                      'has_balcony', 'active', 'construction_year']
+        }),
+        ('Метаданные', {
+            'fields': ['created_at'],
+            'classes': ['collapse']
+        }),
     ]
 
 
